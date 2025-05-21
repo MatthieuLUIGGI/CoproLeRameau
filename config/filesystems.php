fcdf6ef57c4acfaf8e3caf5e812ee3fb5b7b7443
Matthieu Luiggi <matthieu.luiggi@saint-benigne.fr>
2025-05-22 00:07:34 +0200

    Remise fichier

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,15 +47,19 @@ return [
             'report' => false,
         ],
 
-        'disks' => [
-            's3' => [
-                'driver' => 's3',
-                'key' => env('AWS_ACCESS_KEY_ID', null), // Retourne null si non configuré
-                'secret' => env('AWS_SECRET_ACCESS_KEY', null),
-                'region' => env('AWS_DEFAULT_REGION', null),
-                'bucket' => env('AWS_BUCKET', null),
-            ],
+        's3' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+            'report' => false,
         ],
+
     ],
 
     /*
