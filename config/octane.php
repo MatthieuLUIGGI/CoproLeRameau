bff3509560903b3a7426a1245d0348e5fb146a5d
Matthieu Luiggi <matthieu.luiggi@saint-benigne.fr>
2025-05-22 01:49:07 +0200

    test

---
+++ config/octane.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'servers' => [
+        'roadrunner' => [
+            'host' => '0.0.0.0',
+            'port' => env('PORT', 9000),
+        ],
+    ],
+];
--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
     $_SERVER['HTTPS'] = 'on';
+    $_SERVER['SERVER_PORT'] = 443;
 }
 
 use Illuminate\Foundation\Application;
